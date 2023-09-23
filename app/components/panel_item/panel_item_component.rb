8bca8bc5ea82f427c05b44af0e78237a4103f8eb
nmcneany <nmcneany@gmail.com>
2023-09-23 13:54:49 -0500

    css component tweaks

--- app/components/panel_item/panel_item_component.rb
+++ app/components/panel_item/panel_item_component.rb
@@ -4,13 +4,21 @@ module PanelItem
   class PanelItemComponent < ViewComponent::Base
     def initialize(**options)
       @classes = options[:classes]
+      @id = options[:id]
     end
 
     def call
       content_tag(:div,
         content,
-        class: "bg-light rounded border p-1 shadow-sm text-center #{@classes}".strip
+        id: id,
+        class: "bg-light rounded border p-1 shadow-sm text-center #{@classes}".strip,
       )
+    end
+
+    private
+
+    def id
+      @id if @id
     end
   end
 end
