01dfbbcecd96aaab85719c5f1dcfa7e732e05b35
nmcneany <nmcneany@gmail.com>
2023-10-09 14:47:41 -0500

    clear out dev controller

--- app/javascript/controllers/dev_controller.js
+++ app/javascript/controllers/dev_controller.js
@@ -1,27 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 class DevController extends Controller {
-  static targets = [
-    'editor',
-    'editButton'
-  ]
+  static targets = []
 
-  static values = {
-    contentResponse: String
-  }
+  static values = {}
 
-  connect() {
-    console.log('=======>', this.contentResponseValue)
-  }
-
-  trixEditor() {
-    const editor = `
-      <input id="x" value="<h2>Heloooo</h2>" type="hidden" name="content">
-      <trix-editor input="x"></trix-editor>
-    `
-
-    this.editorTarget.innerHTML = editor
-  }
+  connect() {}
 }
 
 export default DevController
